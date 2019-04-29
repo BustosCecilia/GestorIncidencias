5c24831e3e61b5c1cdaa82298e3a6c30078b1b2a
BustosCecilia <ceciliabus@gmail.com>
2019-04-28 21:54:21 -0300

    Se modifico delete de proyectos para que solo se elimine si no tiene incidentes reportados

--- src/main/java/main/SparkRest.java
+++ src/main/java/main/SparkRest.java
@@ -2,6 +2,7 @@ package main;
 
 import com.google.gson.Gson;
 import enums.Clasificacion;
+import enums.Estado;
 import enums.StatusResponse;
 import incidente.Incidente;
 import incidente.IncidenteService;
@@ -15,21 +16,18 @@ import usuario.UsuarioService;
 import usuario.UsuarioServiceMapImpl;
 
 import java.text.ParseException;
-import java.util.HashMap;
-
-import static spark.Spark.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import java.text.DateFormat;
-
-import java.text.SimpleDateFormat;
+import static spark.Spark.*;
 public class SparkRest {
 
     public static void main(String[] args) throws ParseException {
         final IncidenteService incidenteService = new IncidenteServiceMapImpl();
         final UsuarioService usuarioService = new UsuarioServiceMapImpl();
         final ProyectoService proyectoService = new ProyectoServiceMapImpl();
-        port(8586);
+
+        port(8087);
         /** metodos que carga datos en memoria */
         cargarDatos(incidenteService, usuarioService, proyectoService);
 
@@ -68,13 +66,29 @@ public class SparkRest {
 
         delete("/usuario/:id", ((request, response) -> {
             response.type("application/json");
-            int cantidadProyectos=proyectoService.getProyectos().size();
-            for(int i =1;i<=cantidadProyectos;i++){
-                if(usuarioService.getUsuario(request.params(":id"))==proyectoService.getProyecto(String.valueOf(i)).getPropietario()){
+            int cantidadProyectos = proyectoService.getProyectos().size();
+            int cantidadIncidentes = incidenteService.getIncidentes().size();
+            for(int i = 1; i<=cantidadProyectos; i++){
+                if (usuarioService.getUsuario(request.params(":id")) == proyectoService.getProyecto(String.valueOf(i)).
+                        getPropietario()) {
                     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,
                             "usuario es propietario del proyecto id="+i));
                 }
+            }
+            for (int i = 1; i <= cantidadIncidentes; i++) {
+                if (usuarioService.getUsuario(request.params(":id")) == incidenteService.getIncidente(String.valueOf(i)).
+                        getReportador()) {
+                    return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,
+                            "usuario es Reportador del proyecto id=" + i));
+                } else {
+                    if (usuarioService.getUsuario(request.params(":id")) == incidenteService.getIncidente(String.valueOf(i)).
+                            getResponsable()) {
+                        return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,
+                                "usuario es Responsable del proyecto id=" + i));
+                    }
 
+
+                }
             }
             usuarioService.deleteUsuario(request.params(":id"));
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "usuario borrado."));
@@ -87,7 +101,7 @@ public class SparkRest {
                             "El usuario existe." : "El usuario no existe")));
         }));
 
-        /** metodos Proyectos */
+        /** metodos asociado a Proyectos */
         post("/proyecto", (request, response) -> {
             response.type("application/json");
             Proyecto proyecto = new Gson().fromJson(request.body(), Proyecto.class);
@@ -122,6 +136,14 @@ public class SparkRest {
 
         delete("/proyecto/:id", (request, response) -> {
             response.type("application/json");
+            int cantidadIncidentes = incidenteService.getIncidentes().size();
+            for (int i = 1; i <= cantidadIncidentes; i++) {
+                if (proyectoService.getProyecto(request.params(":id")) == incidenteService.getIncidente(String.valueOf(i))
+                        .getProyecto()) {
+                    return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,
+                            "proyecto tiene incidentes reportados"));
+                }
+            }
             proyectoService.deleteProyecto(request.params(":id"));
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "proyecto borrado."));
         });
@@ -133,7 +155,7 @@ public class SparkRest {
                             "El proyecto existe." : "El proyecto no existe")));
         }));
 
-        /** servicios Incidentes*/
+        /** metodos asociados a Incidentes*/
         post("/incidente", (request, response) -> {
             response.type("application/json");
             Incidente incidente = new Gson().fromJson(request.body(), Incidente.class);
@@ -153,17 +175,25 @@ public class SparkRest {
                     new Gson().toJsonTree(incidenteService.getIncidente(request.params(":id")))));
         }));
 
-        put("/incidente/", (request, response) -> { // meter esto en un try catch
+        put("/incidente", (request, response) -> { // meter esto en un try catch
             response.type("application/json");
             Incidente incidente = new Gson().fromJson(request.body(), Incidente.class);
             Incidente incidenteEditado = incidenteService.modifyIncidente(incidente);
             if (incidenteEditado != null) {
                 return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,
-                        new Gson().toJsonTree(incidenteEditado)));
+                        new Gson().toJsonTree(incidenteEditado),
+                        "SOLO SE PUEDE MODIFICAR ESTADO Y AÑADIR TEXTO A DESCRIPCION"));
+
             } else {
                 return new Gson().toJson(new StandardResponse(StatusResponse.ERROR,
                         "Error al editar el incidente"));
             }
+        });
+
+        delete("/incidente/:id", (request, response) -> {
+            response.type("application/json");
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "No se puede eliminar " +
+                    "Incidentes."));
         });
 
         options("/incidente/:id", ((request, response) -> {
@@ -181,6 +211,7 @@ public class SparkRest {
         Usuario usuario = new Usuario("1", "Cecilia", "Bustos");
         Usuario usuario1 = new Usuario("2", "Maria", "Figueroa");
         Usuario usuario2 = new Usuario("3", "Gisela", "Garcia");
+
         usuarioService.addUsuario(usuario);
         usuarioService.addUsuario(usuario1);
         usuarioService.addUsuario(usuario2);
@@ -193,13 +224,15 @@ public class SparkRest {
         SimpleDateFormat d = new SimpleDateFormat("dd-MM-yy");
         Date dateCreacion = d.parse("31-03-2016");
         Date dateSolucion = d.parse("04-04-2016");
-        //System.out.println(DateFormat.getDateInstance().format(date));
-        Incidente incidente = new Incidente("1", Clasificacion.CRITICO,"se perdieron datos",usuario, usuario1,
-                dateCreacion,dateSolucion,proyecto );
-        Incidente incident1 = new Incidente("2", Clasificacion.MENOR,"mejorar solucion",usuario, usuario1,
-                dateCreacion,dateSolucion,proyecto1 );
+
+        Incidente incidente = new Incidente("1", Clasificacion.CRITICO, "se perdieron datos", usuario2, usuario1,
+                dateCreacion, dateSolucion, proyecto, Estado.RESUELTO);
+        Incidente incident1 = new Incidente("2", Clasificacion.MENOR, "mejorar solucion", usuario, usuario,
+                dateCreacion, dateSolucion, proyecto, Estado.ASIGNADO);
+
         incidenteService.addIncidente(incidente);
         incidenteService.addIncidente(incident1);
 
     }
+
 }
